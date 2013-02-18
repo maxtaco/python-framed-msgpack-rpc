d3572ad14d90cf277785842f771f15b636bc0a2b
Maxwell Krohn <themax@gmail.com>
2013-02-18 15:19:56 -0500

    checkpoints

--- fmprpc/dispatch.py
+++ fmprpc/dispatch.py
@@ -18,6 +18,9 @@ class Response (object):
 	def result (self, res): self.__reply(None, res)
 	def error  (self, err): self.__reply(err, None)
 
+	def setDebugMessage(dm):
+		self.debug_msg = dm
+
 	def __reply(self, err, res):
 		if self.debug_msg:
 			self.debug_msg.response(err, res).call()
@@ -33,8 +36,8 @@ class Invocation (object):
 		self.msg = msg
 		self.debug_msg = debug_msg
 		self.notify = notify
-		self.res = None
-		self.err = None
+		self.result = None
+		self.error = None
 		self.complete = False
 
 	def lock(self): self.dispatch._lock
@@ -55,16 +58,23 @@ class Invocation (object):
 			del d._invocations[self.seqid]
 
 		if self.debug_msg:
-			self.debug_msg.response(self.err, self.res).call()
+			self.debug_msg.response(self.error, self.result).call()
 
 		self.lock().release()
-		return [ self.err, self.res ]
+		return [ self.error, self.result ]
 
-	def respond(self,err,res):
+	def respond(self, error=None, result=None):
 		self.complete = True
-		self.err = err
-		self.res = res
+		self.error = error
+		self.resiult = result
 		self.condition.notify()
+
+	##-----------------------------------------
+
+	def cancel (self):
+		self.lock().acquire()
+		self.respond(error = "cancelled")
+		self.lock().release()
 
 ##=======================================================================
 
@@ -77,43 +87,58 @@ class Dispatch (Packetizer):
 	RESPONSE = 1
 	NOTIFY = 2
 
+	##-----------------------------------------
+
 	def __init__ (self):
 		Packetizer.__init__(self)
 		self._invocations = {}
-		self._handlers = {}
+		self._hooks = {}
 		self._seqid = 1
 		self._dbgr = None
 		self._lock = threading.Lock()
 
+	##-----------------------------------------
+
 	def setDebugger (self, d): self._dbgr = d
 
+	##-----------------------------------------
+
+	def __nextSeqid (self):
+		ret = self._seqid
+		self._seqid += 1
+		return ret
+
+	##-----------------------------------------
+
 	def dispatch (self, msg):
+		"""
+		Call this method on an incoming msgpack msg.  Depending on whether it's
+		an Invocation, Notifcation, or Response, it will be routed in the appropriate
+		direction.  In the first two cases, we call down to a subclass for the `serve`
+		method.
+		"""
+
 		self._lock.acquire()
 		if len(msg) < 2:
 			self.__warn("Bad input packet: len={0}".format(len(msg)))
 		else:
 			typ = msg.pop(0)
 			if typ is self.INVOKE:
-				[ seqid, method, param ] = msg
+				[ seqid, method, arg ] = msg
 				response = Response(self, seqid)
-				self.serve (method = method, param = param, response = resposne)
+				self.__serve (method = method, arg = arg, response = resposne)
 			elif typ is self.NOTIFY:
 				[ method, param ] = msg
-				self.serve (method = method, param = param)
+				self.__serve (method = method, arg = arg)
 			elif typ is self.RESPONSE:
 				[ seqid, error, result ] = msg
-				self.__dispatchHandleReponse(seqid = seqid, error = error, result = result)
+				self.__awaken(seqid = seqid, error = error, result = result)
 			else:
 				self.__warn("Unknown message type: {0}".format(typ))
 		self._lock.release()
 
-	def __dispatchHandleResponse (self, seqid, error = None, result = None):
-		self.__awaken(seqid = seqid, error = error, result = result)
 
-	def cancel (self, seqid): 
-		self._lock.acquire()
-		self.__awaken (seqid = seqid, error = "cancelled")
-		self._lock.release()
+	##-----------------------------------------
 
 	def __awaken (self, seqid, error = None, result = None):
 		try:
@@ -122,7 +147,14 @@ class Dispatch (Packetizer):
 		except KeyError:
 			self.__warn("Unknow seqid in awaken: {0}".format(seqid))
 
-	def invoke(self, program=None, method=None, arg=None, notify=False, call=True):
+	##-----------------------------------------
+
+	def __makeMethod (self, prog, meth):
+		".".join([ prog, meth ]) if prog else meth
+
+	##-----------------------------------------
+	
+	def newInvocation(self, program=None, method=None, arg=None, notify=False):
 
 		method = self.__makeMethod(program, method)
 		seqid = self.__nextSeqid()
@@ -147,11 +179,83 @@ class Dispatch (Packetizer):
 					typ = dtyp
 				)
 
-		i = Invocation(self, seqid, msg, debug_msg, notify)
-		if call:
-			ret = i.invoke()
-		else:
-			ret = i
-		return ret
+		return Invocation(self, seqid, msg, debug_msg, notify)
 
+	##-----------------------------------------
+	
+	def invoke (self, program=None, method=None, arg=None, notify=False):
+		i = self.newInvocation(program=progranm, method=method, arg=arg, notify=notify)
+		return i.invoke()
 
+	##-----------------------------------------
+
+	def dispatchReset (self):	
+		"""
+		Reset the dispatcher to its original state.  This cancels all outstanding
+		RPCs.
+		"""
+		invs = self._invocations
+		self._invocations = {}
+		for i in invs.values():
+			i.cancel()
+
+	##-----------------------------------------
+
+	def __serve(self, method, arg, response = None):
+		"""
+		On the server, serve an incoming RPC if a hook is available for the given
+		method.
+		"""
+		hook = self.getHook(method)
+
+		if self._dbgr:
+			seqid = response.seqid if reponse else None
+			debug_msg = self._dbgr.newMessage(
+				method = method,
+				seqid = seqid,
+				arg = arg,
+				dir = debug.constants.dir.INCOMING,
+				remote = self.remoteAddress(),
+				port = self.remotePort(),
+				typ = debug.constants.dir.type.SERVER,
+				error = None if hook else "unknown method"
+				)
+			if response:
+				response.setDebugMessage(debug_msg)
+			debug_msg.call()
+		if hook:
+			hook(method=method, arg=arg, response=response, dispatch=self)
+		elif response:
+			response.error("unknown method: {0}".format(method))
+
+	##-----------------------------------------
+
+	def getHook(self, method): 
+		"""
+		Get the serving hook for the requested method.  You can override this if 
+		you would like.
+		"""
+		return self._hooks.get(method)
+
+	##-----------------------------------------
+
+	def addHandler(self, method, hook, program = None):
+		"""
+		Register a handler hook to handle the given <program>.<method> RPC.
+		"""
+		method = self.__makeMethod(program, method)
+		self._hooks[method] = hook
+
+	##-----------------------------------------
+
+	def addProgram (self, program, hooks):
+		for (method, hook) in hooks.items():
+			self.addHandler(method = method, hook = hook, program = program)
+
+	##-----------------------------------------
+
+	def addPrograms (self, programs):
+		for (program, hooks) in programs.items():
+			self.addProgram(program = program, hooks = hooks)
+
+##=======================================================================

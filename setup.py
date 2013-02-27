99befe53a4af4d81157e4b7702d4cc956182c531
Maxwell Krohn <themax@gmail.com>
2013-02-27 13:31:34 -0500

    a proper setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="framed-msgpack-rpc",
+      version='0.0.1',
+      description="Framed MsgpackRPC client, server and transport libraries",
+      author="Maxwell Krohn",
+      author_email="themax@gmail.com",
+      url="https://github.com/maxtaco/python-framed-msgpack-rpc",
+      packages=[ 'fmprpc' ]
+      )
